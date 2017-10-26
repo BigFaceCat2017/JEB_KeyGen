1d60f160dc63b7e8c82890af9c5e4746e1120e85
qtfreet00 <812298893@qq.com>
2017-10-26 11:34:04 +0800

    优化布局

--- src/KeyGen.java
+++ src/KeyGen.java
@@ -14,15 +14,22 @@ public class KeyGen {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("JEB KeyGen for v2.3.6");
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize(); // 获取屏幕的尺寸
+        int screenWidth = screenSize.width / 2; // 获取屏幕的宽
+        int screenHeight = screenSize.height / 2; // 获取屏幕的高
+        final int height = 150;
+        final int width = 350;
+        frame.setLocation(screenWidth - width / 2, screenHeight - height / 2);
         KeyGen keyGen = new KeyGen();
         frame.setContentPane(keyGen.panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        frame.setSize(new Dimension(350,150));
+        frame.setSize(new Dimension(width, height));
         String key = keygen.KeyGen.getKey(MachineID.get()[0], System.currentTimeMillis() / 1000);
         keyGen.serial.setText(key);
-        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        Clipboard clipboard = kit.getSystemClipboard();
         clipboard.setContents(new StringSelection(key), new ClipboardOwner() {
             @Override
             public void lostOwnership(Clipboard clipboard, Transferable contents) {
